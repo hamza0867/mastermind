54ad52f88b46506f66ac7cbd4abfabb28a8e73f6
hamza0867 <hamza0867@gmail.com>
2019-08-14 15:28:57 +0100

    Clean Home test

--- tests/unit/views/Home.spec.ts
+++ tests/unit/views/Home.spec.ts
@@ -1,39 +1,3 @@
-//import Vuetify, { VTextField } from "vuetify/lib";
-//import Vue from "vue";
-//import { mount, createLocalVue } from "@vue/test-utils";
-//import Vuex, { Store } from "vuex";
-//import Home from "@/views/Home.vue";
-//import { Actions } from "@/store";
-
-//Vue.use(Vuetify);
-//const localVue = createLocalVue();
-//localVue.use(Vuex);
-
-//describe("Home.vue", () => {
-//let actions;
-//let state: { mainPlayer: string };
-//let store: Store<{ mainPlayer: string }>;
-
-//beforeEach(() => {
-//state = {
-//mainPlayer: "Hamza"
-//};
-//actions = {
-//[Actions.updateMainPlayer]: jest.fn()
-//};
-//store = new Vuex.Store({
-//state,
-//actions
-//});
-//});
-
-//it("should show the mainPlayer in inupt field", () => {
-//const HomeComp = mount(Home, { localVue, store, vuetify: new Vuetify() });
-//const textField = HomeComp.find("v-text-field");
-//expect(textField.is(VTextField)).toBe(true);
-//});
-//});
-//
 import Vue from "vue";
 import Vuex, { Store } from "vuex";
 import Vuetify from "vuetify";
