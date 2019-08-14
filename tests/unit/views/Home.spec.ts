d3badf20920ba4c97848bed20a8d5ad54d856d48
hamza0867 <hamza0867@gmail.com>
2019-08-14 15:28:17 +0100

    Finally the test works

--- tests/unit/views/Home.spec.ts
+++ tests/unit/views/Home.spec.ts
@@ -1,18 +1,55 @@
-import Vuetify, { VTextField } from "vuetify/lib";
+//import Vuetify, { VTextField } from "vuetify/lib";
+//import Vue from "vue";
+//import { mount, createLocalVue } from "@vue/test-utils";
+//import Vuex, { Store } from "vuex";
+//import Home from "@/views/Home.vue";
+//import { Actions } from "@/store";
+
+//Vue.use(Vuetify);
+//const localVue = createLocalVue();
+//localVue.use(Vuex);
+
+//describe("Home.vue", () => {
+//let actions;
+//let state: { mainPlayer: string };
+//let store: Store<{ mainPlayer: string }>;
+
+//beforeEach(() => {
+//state = {
+//mainPlayer: "Hamza"
+//};
+//actions = {
+//[Actions.updateMainPlayer]: jest.fn()
+//};
+//store = new Vuex.Store({
+//state,
+//actions
+//});
+//});
+
+//it("should show the mainPlayer in inupt field", () => {
+//const HomeComp = mount(Home, { localVue, store, vuetify: new Vuetify() });
+//const textField = HomeComp.find("v-text-field");
+//expect(textField.is(VTextField)).toBe(true);
+//});
+//});
+//
 import Vue from "vue";
-import vuetify from "@/plugins/vuetify";
-import { shallowMount, mount } from "@vue/test-utils";
 import Vuex, { Store } from "vuex";
+import Vuetify from "vuetify";
 import Home from "@/views/Home.vue";
+import { createLocalVue, mount } from "@vue/test-utils";
 import { Actions } from "@/store";
 
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
 Vue.use(Vuetify);
 
-describe("Home.vue", () => {
-  let actions;
-  let state: { mainPlayer: string };
-  let store: Store<{ mainPlayer: string }>;
-
+let state: { mainPlayer: string };
+let actions;
+let store: Store<{ mainPlayer: string }>;
+describe("Home", () => {
   beforeEach(() => {
     state = {
       mainPlayer: "Hamza"
@@ -25,10 +62,15 @@ describe("Home.vue", () => {
       actions
     });
   });
-
-  it("should show the mainPlayer in inupt field", () => {
-    const HomeComp = shallowMount(Home, { store });
-    const textField = HomeComp.find("v-text-field");
-    expect(textField.is(VTextField)).toBe(true);
+  it("should render properly", () => {
+    const wrapper = mount(Home, {
+      localVue,
+      store,
+      vuetify: new Vuetify()
+    });
+    Vue.nextTick().then(() => {
+      const inputField = wrapper.find("input").element as HTMLInputElement;
+      expect(inputField.value).toBe("Hamza");
+    });
   });
 });
